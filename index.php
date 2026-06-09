d90ba3e023e384daef63daa415ada1c7c333e276
Attths <hafishathallah0109@gmail.com>
2026-06-09 15:16:32 +0700

    build & styling login page

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adventure Time Wiki</title>
+    <link rel="stylesheet" href="public/assests/css/login.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mochiy+Pop+One&family=Outfit:wght@100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+
+    <header>
+        <img src="public/assests/pics/logo.png" width="200px" alt="Logo Adventure Time">
+    </header>
+
+    <main>
+        <div class="login-title">
+            <h1 class="font-style">Welcome To Wiki Fandom</h1>
+            <h2 class="font-style">Sign In Now</h2>
+        </div>
+
+        <div class="login-box">
+            <div class="login-form">
+                <form action="">
+                    <label for="username">Username</label>
+                    <input type="text" name="username" id="username" placeholder="Enter Your Username">
+
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" placeholder="Enter Your Password">
+
+                    <input type="submit" name="masuk" id="masuk" value="Sign In" class="font-style">
+                    <input type="submit" name="guest" id="guest" value="Sign In As Guest" class="font-style">
+                </form>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
