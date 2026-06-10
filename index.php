0cf82d2d3e7ca63a59734dd613e796db77f1bc29
Attths <hafishathallah0109@gmail.com>
2026-06-10 18:04:06 +0700

    bikin homepage

--- index.php
+++ index.php
@@ -4,35 +4,65 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Adventure Time Wiki</title>
-    <link rel="stylesheet" href="public/assests/css/login.css">
+    <link rel="stylesheet" href="public/assests/css/index.css">
+    <link rel="stylesheet" href="public/assests/css/style.css">
+    <script src="https://kit.fontawesome.com/1c755e7c13.js" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Mochiy+Pop+One&family=Outfit:wght@100..900&display=swap" rel="stylesheet">
+
 </head>
 <body>
-
-    <header>
-        <img src="public/assests/pics/logo.png" width="200px" alt="Logo Adventure Time">
-    </header>
+    <nav>
+        <div class="nav-logo">
+            <a href="#">
+                <img src="public/assests/pics/logo.png" alt="Logo Adventure Time" width="130px">
+            </a>
+        </div>
+        
+        <div class="nav-option">
+            <ul>
+                <li class="font-style"><a href="">Character</a></li>
+                <li class="font-style"><a href="">Items</a></li>
+                <li class="font-style"><a href="">World Map</a></li>
+                <li class="font-style"><a href="">Storyline</a></li>
+            </ul>
+        </div>
+        
+        <div class="nav-account">
+            <i class="fa-solid fa-circle-user fa-2x" style="color: white;"></i>
+        </div>
+    </nav>
 
     <main>
-        <div class="login-title">
-            <h1 class="font-style">Welcome To Wiki Fandom</h1>
-            <h2 class="font-style">Sign In Now</h2>
+        <div class="page-header">
+            <h2>Welcome To The</h2>
+            <h1>Land Of Ooo</h1>
         </div>
 
-        <div class="login-box">
-            <div class="login-form">
-                <form action="">
-                    <label for="username">Username</label>
-                    <input type="text" name="username" id="username" placeholder="Enter Your Username">
+        <div class="menu-container">
+            <div class="menu-card" style="--card-color:#EFE467;">
+                <a href="#">
+                    <img src="public/assests/pics/icon-finn.png" alt="Icon Character" width="50px"> Character
+                </a>
+            </div>
 
-                    <label for="password">Password</label>
-                    <input type="password" name="password" id="password" placeholder="Enter Your Password">
+            <div class="menu-card" style="--card-color:#F6A5C0;">
+                <a href="#">
+                    <img src="public/assests/pics/icon-crown.png" alt="Icon Character" width="50px"> Items
+                </a>
+            </div>
 
-                    <input type="submit" name="masuk" id="masuk" value="Sign In" class="font-style">
-                    <input type="submit" name="guest" id="guest" value="Sign In As Guest" class="font-style">
-                </form>
+            <div class="menu-card" style="--card-color:#4BE462;">
+                <a href="#">
+                    <img src="public/assests/pics/icon-map.png" alt="Icon Character" width="50px"> World Map
+                </a>
+            </div>
+
+            <div class="menu-card" style="--card-color:#4C58C0;">
+                <a href="#">
+                    <img src="public/assests/pics/icon-book.png" alt="Icon Character" width="50px"> Storyline
+                </a>
             </div>
         </div>
     </main>
