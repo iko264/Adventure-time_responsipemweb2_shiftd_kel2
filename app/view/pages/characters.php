8fba7c1f15e04b0979a00a8a1d40b136b13e1808
Attths <hafishathallah0109@gmail.com>
2026-06-10 20:48:50 +0700

    rapihin css, bikin page character, bikin data json buat character

---
+++ app/view/pages/characters.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adventure Time Wiki</title>
+    <link rel="stylesheet" href="../../../public/assests/css/characters.css">
+    <link rel="stylesheet" href="../../../public/assests/css/style.css">
+    <script src="https://kit.fontawesome.com/1c755e7c13.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mochiy+Pop+One&family=Outfit:wght@100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <nav>
+        <div class="nav-logo">
+            <a href="../../../index.php">
+                <img src="../../../public/assests/pics/logo.png" alt="Logo Adventure Time" width="130px">
+            </a>
+        </div>
+        
+        <div class="nav-option">
+            <ul>
+                <li class="font-style"><a href="">Character</a></li>
+                <li class="font-style"><a href="">Items</a></li>
+                <li class="font-style"><a href="">World Map</a></li>
+                <li class="font-style"><a href="">Storyline</a></li>
+            </ul>
+        </div>
+        
+        <div class="nav-account">
+            <i class="fa-solid fa-circle-user fa-2x" style="color: white;"></i>
+        </div>
+    </nav>
+
+    <main>
+    <h1 class="page-header">Characters</h1>
+
+    <section class="character-select-container">
+        <div class="character-select-bg">
+
+            <div class="character-grid">
+                <a href="#" class="character-card">
+                    <img src="../../../public/assests/pics/finn.png" alt="Finn" width="50px">
+                    <h3>Finn the Human</h3>
+                </a>
+
+                <a href="#" class="character-card">
+                    <img src="../../../public/assests/pics/bubblegum.png" alt="Bubblegum">
+                    <h3>Princess Bubblegum</h3>
+                </a>
+
+                <a href="#" class="character-card add-card">
+                    <i class="fa-solid fa-plus"></i>
+                    <h3>Add Character</h3>
+                </a>
+
+            </div>
+
+        </div>
+    </section>
+</main>
+</body>
+</html>
--- index.php
+++ index.php
@@ -41,25 +41,25 @@
         </div>
 
         <div class="menu-container">
-            <div class="menu-card" style="--card-color:#EFE467;">
-                <a href="#">
+            <div class="menu-card" style="--card-color:#EFE467; border: 2.5px solid #baaf3b;">
+                <a href="app/view/pages/characters.php">
                     <img src="public/assests/pics/icon-finn.png" alt="Icon Character" width="50px"> Character
                 </a>
             </div>
 
-            <div class="menu-card" style="--card-color:#F6A5C0;">
+            <div class="menu-card" style="--card-color:#F6A5C0; border: 2.5px solid #ce6487;">
                 <a href="#">
                     <img src="public/assests/pics/icon-crown.png" alt="Icon Character" width="50px"> Items
                 </a>
             </div>
 
-            <div class="menu-card" style="--card-color:#4BE462;">
+            <div class="menu-card" style="--card-color:#4BE462; border: 2.5px solid #27b93d;">
                 <a href="#">
                     <img src="public/assests/pics/icon-map.png" alt="Icon Character" width="50px"> World Map
                 </a>
             </div>
 
-            <div class="menu-card" style="--card-color:#4C58C0;">
+            <div class="menu-card" style="--card-color:#6773df; border: 2.5px solid #4C58C0;">
                 <a href="#">
                     <img src="public/assests/pics/icon-book.png" alt="Icon Character" width="50px"> Storyline
                 </a>
